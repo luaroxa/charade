f81c4b9788fb27e4e445c04a1028f1084f9cc08d
luaroxa <luaroxadance@gmail.com>
2021-09-13 15:44:42 -0600

    cleaner html and css

--- main.js
+++ main.js
@@ -3,7 +3,42 @@ let score;
 let playerTurn;
 let winner;
 let cardContent;
+let scoreA;
+let scoreB;
+let intro = ['']
 
+
+
+const hard = ['Compass', 'Crystal', 'cup', 'Diamond','Fan','Game']
+const medium = ['Fruit', 'Fungus', 'Film', 'Ice','Garden', 'Gate']
+const easy = ['Mist', 'Needle', 'Onion', 'Pants', 'Rainbow', 'Snail']
 
 let scoreBoard = document.getElementById('score-board')
 let mainCard = document.getElementById('card#2')
+
+
+function init(){
+    playerTurn = 1 //player 1 
+    intro = [
+        ['choose']
+        ['your']
+        ['words!']
+    ]
+}
+
+intro = [hard, med, easy];
+
+
+function genHard() {
+    return hard[Math.floor(Math.random() * 6)]
+  }
+
+function genMedium() {
+    return hard[Math.floor(Math.random() * 6)]
+  }
+
+function genEasy() {
+    return hard[Math.floor(Math.random() * 6)]
+  }
+
+  init()
