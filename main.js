79d0fd15dbc8ca10173f76e87805e513a8cfc785
luaroxa <luaroxadance@gmail.com>
2021-09-13 13:56:14 -0600

    first draft of html set up

---
+++ main.js
@@ -0,0 +1,9 @@
+//set 3 main variable
+let score;
+let playerTurn;
+let winner;
+let cardContent;
+
+
+let scoreBoard = document.getElementById('score-board')
+let mainCard = document.getElementById('card#2')
